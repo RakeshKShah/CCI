baf87754438d88860efffa4d969aeed81ab48044
Rakesh Kumar Shah <rakesh.shah.iiita@gmail.com>
2017-12-24 19:30:09 -0600

    One Away

---
+++ OneAway/src/oneaway/OneAway.java
@@ -0,0 +1,20 @@
+package oneaway;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rakesh
+ */
+public class OneAway {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String strA = sc.nextLine();
+       
+    }
+    
+}
